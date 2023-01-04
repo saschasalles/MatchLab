6b5dbba45835592297c57047da7f288439a1426d
Sascha Salles <sascha@luni.app>
2023-01-04 02:27:34 +0100

    Add Swipe (with Swipe Gesture)

--- MatchLab/Feed/FeedViewController.swift
+++ MatchLab/Feed/FeedViewController.swift
@@ -15,6 +15,11 @@ final class FeedViewController: UIViewController {
 
     private var count = 0
 
+    fileprivate enum SwipeDirection {
+        case left
+        case right
+    }
+
     // MARK: - UI Attributes
 
     @IBOutlet private weak var imageView: UIImageView!
@@ -30,6 +35,26 @@ final class FeedViewController: UIViewController {
     @IBOutlet private weak var matchButton: UIButton!
 
     @IBOutlet private weak var refuseButton: UIButton!
+
+    private lazy var panGestureRecognizer: UIPanGestureRecognizer = {
+        let recog = UIPanGestureRecognizer(target: self, action: #selector(dragCard(_:)))
+
+        return recog
+    }()
+
+    private lazy var swipeRightGestureRecognizer: UISwipeGestureRecognizer = {
+        let recog = UISwipeGestureRecognizer(target: self, action: #selector(didRightSwipeCard(_:)))
+        recog.direction = .right
+        return recog
+    }()
+
+    private lazy var swipeLeftGestureRecognizer: UISwipeGestureRecognizer = {
+        let recog = UISwipeGestureRecognizer(target: self, action: #selector(didLeftSwipeCard(_:)))
+        recog.direction = .left
+        return recog
+    }()
+
+    private lazy var hapticGenerator = UIImpactFeedbackGenerator(style: .soft)
 
     // MARK: - UI Methods
 
@@ -51,7 +76,6 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(#function, self)
         configureUI()
         nextProfile()
     }
@@ -63,6 +87,7 @@ final class FeedViewController: UIViewController {
         containerStackView.clipsToBounds = true
         containerStackView.layer.cornerCurve = .continuous
 
+        imageView.isUserInteractionEnabled = true
         imageView.layer.cornerRadius = 20
         imageView.layer.cornerCurve = .continuous
         imageView.contentMode = .scaleAspectFill
@@ -70,6 +95,10 @@ final class FeedViewController: UIViewController {
         bannerView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
         bannerView.layer.cornerRadius = 20
         bannerView.layer.cornerCurve = .continuous
+
+        imageView.addGestureRecognizer(swipeLeftGestureRecognizer)
+        imageView.addGestureRecognizer(swipeRightGestureRecognizer)
+        hapticGenerator.prepare()
     }
 
     private func nextProfile() {
@@ -95,16 +124,136 @@ final class FeedViewController: UIViewController {
         UIView.transition(
             with: imageView,
             duration: 0.35,
-            options: .transitionCurlUp,
+            options: .transitionCrossDissolve,
             animations: { [weak self] in
                 self?.imageView.image = UIImage(systemName: "smiley")
                 self?.imageView.contentMode = .scaleAspectFit
+                self?.hapticGenerator.impactOccurred(intensity: 1.0)
             }
         )
-
+        imageView.isUserInteractionEnabled = false
         matchButton.isEnabled = false
         refuseButton.isEnabled = false
         bannerView.isHidden = true
     }
 
+    @objc func dragCard(_ sender: UIPanGestureRecognizer) {
+        guard let senderView = sender.view  else {
+            print("no data")
+            return
+        }
+
+        let translation = sender.translation(in: senderView.superview)
+
+        switch sender.state {
+        case .began:
+            print("- began -")
+            hideBanner()
+        case .changed:
+
+            let rotationTransform = CGAffineTransform(rotationAngle: translation.x * .pi / 180)
+            senderView.transform = rotationTransform
+
+        case .failed, .cancelled:
+            showBanner { [weak self] in
+                self?.resetCardPosition(senderView)
+            }
+        case .possible:
+            print("possible")
+        case .ended:
+            showBanner { [weak self] in
+                self?.resetCardPosition(senderView)
+            }
+        @unknown default:
+            return
+        }
+    }
+
+    @objc func didRightSwipeCard(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            swipeCard(direction: .right)
+        }
+    }
+
+    @objc func didLeftSwipeCard(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            swipeCard(direction: .left)
+        }
+    }
+
+    private func swipeCard(direction: SwipeDirection) {
+        UIView.animate(
+            withDuration: 0.15,
+            delay: .zero,
+            options: [.curveEaseIn],
+            animations: { [weak self] in
+                guard let self else { return }
+                self.containerStackView.isUserInteractionEnabled = false
+                self.bannerView.alpha = .zero
+                self.imageView.alpha = .zero
+                let midScreenTranslate = self.view.bounds.midX
+                let transform = CGAffineTransform(
+                    translationX: direction == .left ? -midScreenTranslate : midScreenTranslate,
+                    y: 0
+                )
+
+                var rotation = CGAffineTransform(
+                    rotationAngle: 10 * .pi / 180
+                )
+
+                rotation = direction == .left ? rotation.inverted() : rotation
+
+                self.bannerView.transform = transform.concatenating(rotation)
+                self.imageView.transform = transform.concatenating(rotation)
+            },
+            completion: { [weak self] _ in
+                guard let self else { return }
+
+                if direction == .right { self.matchProfile() }
+                self.nextProfile()
+
+                self.containerStackView.isUserInteractionEnabled = true
+                self.bannerView.alpha = 1
+                self.imageView.alpha = 1
+                self.imageView.isHidden = false
+                self.imageView.transform = .identity
+                self.bannerView.transform = .identity
+                self.hapticGenerator.impactOccurred()
+            }
+        )
+    }
+
+    private func resetCardPosition(_ card: UIView) {
+        UIView.animate(withDuration: 0.1) {
+            card.transform = .identity
+        }
+    }
+
+    private func showBanner(completionHandler: @escaping () -> Void) {
+        UIView.animate(
+            withDuration: 0.1,
+            animations: { [weak self] in
+                self?.bannerView.alpha = 1
+                self?.bannerView.isHidden = false
+            },
+            completion: { _ in
+                completionHandler()
+            }
+        )
+    }
+
+    private func hideBanner(completionHandler: (() -> Void)? = nil) {
+        UIView.animate(
+            withDuration: 0.1,
+            delay: .zero,
+            options: [.curveLinear],
+            animations: { [weak self] in
+                self?.bannerView.alpha = 0
+            },
+            completion: { [weak self] _ in
+                self?.bannerView.isHidden = true
+                completionHandler?()
+            }
+        )
+    }
 }
